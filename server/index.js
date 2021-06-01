ce4641598502d436cd4582a6aaa414703706cab5
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-06-01 10:17:13 -0700

    Add code to run socket.io

--- server/index.js
+++ server/index.js
@@ -5,6 +5,11 @@ const io = require("socket.io")(server);
 
 const PORT = process.env.PORT || 5000;
 
+io.on("connection", (socket) => {
+  const { id } = socket.client;
+  console.log(`User connected: ${id}`);
+});
+
 server.listen(PORT, () => {
   console.log(`Listening on *: ${PORT}`);
 });
