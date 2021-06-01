15afbee3548022b3bb252aba389828008f16f719
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-06-01 10:16:47 -0700

    Create server

---
+++ server/index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Listening on *: ${PORT}`);
+});
