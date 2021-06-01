45ea2a3a560cde1ddb210618d2c873a1b651110a
Joshua Gomberoff <joshuagomberoff@gmail.com>
2021-06-01 10:55:19 -0700

    Emit socket "chat message" on submit

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import io from "socket.io-client";
 
 const socket = io.connect("http://localhost:5000");
 
 function App() {
-  return <div />;
+  const [msg, setMsg] = useState({ msg: "" });
+
+  const onTextChange = (e) => {
+    setMsg({ msg: e.target.value });
+  };
+
+  const onMessageSubmit = () => {
+    socket.emit("chat message", msg);
+    setMsg({ msg: "" });
+  };
+
+  return (
+    <div>
+      <input onChange={(e) => onTextChange(e)} value={msg.msg} />
+      <button onClick={onMessageSubmit}>Send</button>
+    </div>
+  );
 }
 
 export default App;
